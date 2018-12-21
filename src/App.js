1ab0241fe603e6886d38e1fa8c3ad8f7c1f80498
IonutAlexandru <cosuleanu@fazua.com>
2018-12-21 16:43:10 +0100

    conflictooo

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> Adiós.
+            Edit <code>src/App.js</code> Olll.
           </p>
           <a
             className="App-link"
