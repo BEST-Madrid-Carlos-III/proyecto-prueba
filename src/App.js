f0db64c565684684d175edf4de3c164958b82b86
IonutAlexandru <cosuleanu@fazua.com>
2018-12-21 16:21:35 +0100

    Commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> Hola.
+            Edit <code>src/App.js</code> Adiós.
           </p>
           <a
             className="App-link"
